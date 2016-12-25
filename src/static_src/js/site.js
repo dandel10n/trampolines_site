35c63109e7c89243acdc9650a93864c21975f63b
Mariya Radchuk <radchuk.mary@gmail.com>
2016-12-25 14:24:04 +0300

    Add mobile version of sidebar

--- src/static_src/js/site.js
+++ src/static_src/js/site.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
         $(".mods-options").toggleClass("mods-options__active");
         $(".full-info").toggleClass("full-info__active");
     });
+
+    $(".choose-diameter").click(function() {
+        $(".choose-diameter_options").toggleClass("choose-diameter_options__active");
+    });
+
+    $(".choose-diameter_options").click(function() {
+        $(".choose-diameter_options").removeClass("choose-diameter_options__active");
+    });
 });
